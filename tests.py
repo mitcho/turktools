a06c418fa9051570bc0c45b1d91a183737c16f53
Michael Yoshitaka Erlewine <mitcho@mitcho.com>
2013-05-28 21:33:35 -0400

    tests: mv test_decoder test_csv, and make it test lister.graceful_write_csv too

--- tests.py
+++ tests.py
@@ -11,11 +11,10 @@ See readme for license block
 from __future__ import print_function
 import unittest, sys
 # todo: import doctest
-import tests.test_decoder
 
 runner = unittest.TextTestRunner(verbosity=1 + sys.argv.count('-v'))
 suite = unittest.TestLoader().loadTestsFromNames([
-	'tests.test_decoder',
+	'tests.test_csv',
 	'tests.test_lister',
 ])
 raise SystemExit(not runner.run(suite).wasSuccessful())
